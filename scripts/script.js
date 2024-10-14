9a3e7887e61b47addc48db7c89e930c48cf30c69
Yuga <xyugaz@gmail.com>
2024-10-14 22:52:35 +0900

    update

--- scripts/script.js
+++ scripts/script.js
@@ -58,21 +58,70 @@ for (let i = 0; i < filterBtn.length; i++) {
   });
 }
 
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
+const sideNavigationItems = document.querySelectorAll("[data-side-nav]");
+const homeNavigationItems = document.querySelectorAll("[data-home-nav]");
+const pageItems = document.querySelectorAll("[data-page-type]");
+let lastPageItem = homeNavigationItems[1].dataset.homeNav;
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
+for (const homeNavItem of homeNavigationItems) {
+  homeNavItem.addEventListener("click", () => {
+    for (const item of homeNavigationItems) {
+      if (item == homeNavItem) {
+        item.classList.add("active");
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        item.classList.remove("active");
+      }
+    }
+    for (const sideNavItem of sideNavigationItems) {
+      if (sideNavItem.dataset.sideNav != "home") {
+        sideNavItem.classList.remove("active");
+      } else {
+        sideNavItem.classList.add("active");
+      }
+    }
+    for (const page of pageItems) {
+      if (page.dataset.page == homeNavItem.dataset.homeNav) {
+        page.classList.add("active");
+        lastPageItem = page.dataset.page;
+      } else {
+        page.classList.remove("active");
+      }
+    }
+  });
+}
+
+for (const sideNavItem of sideNavigationItems) {
+  sideNavItem.addEventListener("click", () => {
+    for (const item of sideNavigationItems) {
+      if (item == sideNavItem) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove("active");
+      }
+    }
+    if (sideNavItem.dataset.sideNav == "home") {
+      for (const page of pageItems) {
+        if (page.dataset.page == lastPageItem) {
+          page.classList.add("active");
+        } else {
+          page.classList.remove("active");
+        }
+      }
+      for (const homeNavItem of homeNavigationItems) {
+        if (homeNavItem.dataset.homeNav == lastPageItem) {
+          homeNavItem.classList.add("active");
+        }
+      }
+    } else {
+      for (const homeNavItem of homeNavigationItems) {
+        homeNavItem.classList.remove("active");
+      }
+      for (const page of pageItems) {
+        if (page.dataset.pageType == sideNavItem.dataset.sideNav) {
+          page.classList.add("active");
+        } else {
+          page.classList.remove("active");
+        }
       }
     }
   });
