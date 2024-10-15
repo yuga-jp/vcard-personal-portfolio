de1ffb54f913ccb6ae09054fe3e7d129ea018c37
Yuga <xyugaz@gmail.com>
2024-10-15 16:43:02 +0900

    Remove unnecessary code

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,6 @@
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-// sidebar variables
-const sidebar = document.querySelector("[data-sidebar]");
-const sidebarBtn = document.querySelector("[data-sidebar-btn]");
-
-// sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
